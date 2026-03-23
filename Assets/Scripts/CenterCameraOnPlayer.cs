afbd3f2dab6f1f10e420b816dcfdee633b73034b
Octavian-Tiberiu NISTORA <octavian.nistora@gmail.com>
2026-03-24 00:39:49 +0200

    Added simple testing level and simple movement mechanics

---
+++ Assets/Scripts/CenterCameraOnPlayer.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class CenterCameraOnPlayer : MonoBehaviour
+{
+    [SerializeField] private Camera camera;
+
+    private void Start()
+    {
+        if (camera == null)
+        {
+            camera = Camera.main;
+        }
+        if (camera == null)
+        {
+            Debug.LogError("No camera found to center on player.");
+            return;
+        }
+
+        var cameraPositionConstraint = camera.gameObject.GetComponent<PositionConstraint>();
+        if (!cameraPositionConstraint)
+        {
+            cameraPositionConstraint = camera.gameObject.AddComponent<PositionConstraint>();
+        }
+        
+        ConfigureConstraint(cameraPositionConstraint);
+    }
+
+    private void ConfigureConstraint(PositionConstraint positionConstraint)
+    {
+        positionConstraint.constraintActive = true;
+        positionConstraint.weight = 1;
+        
+        positionConstraint.locked = true;
+        positionConstraint.translationOffset = Vector3.back;
+        positionConstraint.translationAxis = Axis.X | Axis.Y | Axis.Z;
+        
+        var constraintSourceList = new List<ConstraintSource>(new[]
+        {
+            new ConstraintSource
+            {
+                sourceTransform = transform,
+                weight = 1
+            }
+        });
+        positionConstraint.SetSources(constraintSourceList);
+    }
+}
